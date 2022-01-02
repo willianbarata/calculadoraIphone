79f50e8829675292084085948eacc570c373c6e3
Willian Barata <willianbarata@gmail.com>
2022-01-02 09:28:32 -0300

    concluindo layout dos botões

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
+import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+class Botao extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {};
+
+    let c = 1;
+    if(props.c){
+      c = parseInt(props.c);
+    }
+
+    this.styles = StyleSheet.create({
+      area:{
+        flex: c,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#999999',
+        backgroundColor: '#E0E0E0'
+      },
+      text:{
+        fontSize: 18
+      }
+    })
+  } 
+  render(){
+    return(
+      <TouchableOpacity style={this.styles.area}>
+        <Text style={this.styles.text}></Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+export default class calculadoraIphone extends Component {
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <View style={styles.linha}>
+        <Botao />
+        <Botao />
+        <Botao />
+      </View>
+      <View style={styles.linha}>
+        <Botao />
+        <Botao />
+        <Botao />
+      </View>
+      <View style={styles.linha}>
+        <Botao />
+        <Botao />
+        <Botao />
+      </View>
+      <View style={styles.linha}>
+        <Botao />
+        <Botao />
+        <Botao />
+      </View>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body: {
+    paddingTop: 20,
+    flex: 1
   },
+  linha:{
+    flex: 1,
+    flexDirection: 'row'
+  }
 });
